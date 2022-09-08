24dfd13dfd6327a3d9d5d351609c2cfca132565d
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-09-08 16:17:02 -0700

    fixed photo

--- js/script.js
+++ js/script.js
@@ -1,33 +1,23 @@
-/*let lastScrollPosition = 0;
-document.addEventListener('scroll', (event) => {
-  let navbar = document.getElementById("navigation");
-  if (window.scrollY > lastScrollPosition) {
-    navbar.remove("fixed-top");
-    navbar.classList.add("fixed-top");
-  } else {
-    navbar.classList.add("fixed-top");
-  }
-  lastScrollPosition = window.scrollY;
-}); */
-
-// Enable hidden nav bar
+// Code chunk is responsible for fade out/fade in of nav bar
 {
   const nav = document.getElementById("navigation");
+  // window.scrollY gives number of pixel someone has scrolled down from the top of the page
+  // initially is null
   let lastScrollY = window.scrollY;
 
   window.addEventListener("scroll", () => {
     if (lastScrollY < window.scrollY) {
-      //nav.classList.add("navigation--hidden")
-      //alert(nav.offsetHeight);
-      let h = nav.offsetHeight;
-      nav.style.transform = `translateY(${-h}px)`;
+      // get height of nav bar
+      let height = nav.offsetHeight;
+      // push navbar out of the screen
+      nav.style.transform = `translateY(${-height}px)`;
     } else {
-      //nav.classList.remove("navigation--hidden");
-      let h = nav.offsetHeight;
+      // put navbar back on to the screen
+      let height = nav.offsetHeight;
       nav.style.transform = `translateY(${0}px)`;
 
     }
-
+    // set new Ausgangswert für scroll
     lastScrollY = window.scrollY;
   });
 }
